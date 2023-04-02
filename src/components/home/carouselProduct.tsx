107ce3c851e2d41a301e8ba2c80913a4f1c14c95
dewkul <jirapat0811@gmail.com>
2023-04-02 13:55:19 +0700

    feat(home): add carousel

---
+++ src/components/home/carouselProduct.tsx
@@ -0,0 +1,42 @@
+import { Button, Carousel } from "flowbite-react"
+
+export default function ProductCarousel({ urls }: CarouselProps) {
+    return (
+        <Carousel>
+            {
+                urls.map((url, i) => (i === urls.length - 1)
+                    ? <LastItemCarousel url={url} /> : <ItemCarousel url={url} />)
+            }
+        </Carousel>
+    )
+}
+
+
+function ItemCarousel({ url }: ItemCarouselProps) {
+    return (
+        <div>
+            <img src={url} />
+        </div>
+    )
+}
+
+function LastItemCarousel({ url }: ItemCarouselProps) {
+    return (
+        <div>
+            <div class="z-10 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+                <p class="uppercase">new collection</p>
+                <Button color="light" outline={true}>Shop Now</Button>
+            </div>
+            <img src={url} />
+        </div>
+    )
+}
+
+
+interface CarouselProps {
+    urls: string[]
+}
+
+interface ItemCarouselProps {
+    url: string
+}
