535b38363988157dbdcba4f3589869f598ffd28b
dewkul <jirapat0811@gmail.com>
2023-04-03 23:11:01 +0700

    refac(product): card style

--- src/components/product/briefCardProduct.tsx
+++ src/components/product/briefCardProduct.tsx
@@ -1,15 +1,16 @@
+import { Card, Button } from "flowbite-react";
 import { Product } from "../../schema/product";
 
 
 export default function BriefProductCard({ product }: Props) {
     const { name, discountedPrice, media } = product
     return (
-        <div class="rounded drop-shadow-xl bg-white">
+        <Card>
             <img src={media[0].url} class="h-40 md:h-52 xl:h-60" />
-            <div>{name}</div>
-            <div>{discountedPrice}</div>
-            <button class="outline w-full ">Add to cart</button>
-        </div>
+            <h4>{name}</h4>
+            <h3>{discountedPrice}</h3>
+            <Button class=" w-full">Add to cart</Button>
+        </Card>
     )
 }
 
