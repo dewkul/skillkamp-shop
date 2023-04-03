9f775c98cd0696df3a41a12298a6b5a10c432381
dewkul <jirapat0811@gmail.com>
2023-04-03 18:02:06 +0700

    feat(contact): form send mail

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,4 +1,5 @@
 import { Card } from "flowbite-react"
+import { useState } from "preact/hooks"
 
 export default function ContactPage(_: Props) {
     const contacts = [
@@ -56,8 +57,32 @@ function ContentGroup({ title, lines }: ContentGroupProps) {
     )
 }
 
-
 function ContactForm() {
+    const [subject, setSubject] = useState("")
+    const [email, setEmail] = useState("")
+    const [message, setMessage] = useState("")
+
+
+    const onEmailInput = (e: Event) => {
+        if (e.target instanceof HTMLInputElement)
+            setEmail(e.target.value)
+    }
+
+    const onSubjectInput = (e: Event) => {
+        if (e.target instanceof HTMLInputElement)
+            setSubject(e.target.value)
+    }
+
+    const onMessageInput = (e: Event) => {
+        if (e.target instanceof HTMLInputElement)
+            setMessage(e.target.value)
+    }
+
+    const sendMail = () => {
+        // TODO: fix 
+        window.open(`mailto:${email}?${subject ? `subject=${subject}` : ""}${message ? `&body=${message}` : ""}`)
+    }
+
     return (
         <Card >
             <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
@@ -67,13 +92,12 @@ function ContactForm() {
                 For any questions, please send us a message
             </p>
             {/* <form> */}
-            <div class="flex flex-wrap -mx-3 mb-6">
+            <div class="flex flex-wrap -mx-3 mb-2">
                 <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
                     <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
                         First Name
                     </label>
-                    <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="text" placeholder="Jane" />
-                    <p class="text-red-500 text-xs italic">Please fill out this field.</p>
+                    <input class="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" id="grid-first-name" type="text" placeholder="Jane" />
                 </div>
                 <div class="w-full md:w-1/2 px-3">
                     <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-last-name">
@@ -82,32 +106,48 @@ function ContactForm() {
                     <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" type="text" placeholder="Doe" />
                 </div>
             </div>
-            <div class="flex flex-wrap -mx-3 mb-6">
+            <div class="flex flex-wrap -mx-3 mb-2">
                 <div class="w-full px-3">
-                    <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-password">
+                    <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-email">
                         Email (*required)
                     </label>
-                    <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-password" type="password" placeholder="******************" />
-
+                    <input
+                        value={email}
+                        onInput={onEmailInput}
+                        class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-email" type="email" placeholder="your@email.com"
+                        required
+                    />
+                    {/* <p class="text-red-500 text-xs italic">Please fill out this field.</p> */}
                 </div>
             </div>
-            <div class="flex flex-wrap -mx-3 mb-6">
+            <div class="flex flex-wrap -mx-3 mb-2">
                 <div class="w-full px-3">
-                    <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-password">
+                    <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-subject">
                         Subject
                     </label>
-                    <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-password" type="password" placeholder="******************" />
+                    <input
+                        value={subject}
+                        onInput={onSubjectInput}
+                        class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-subject" type="text" placeholder="Your title" />
 
                 </div>
             </div>
-            <div class="flex flex-wrap -mx-3 mb-6">
+            <div class="flex flex-wrap -mx-3">
                 <div class="w-full px-3">
-                    <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-password">
+                    <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-msg">
                         Message
                     </label>
-                    <textarea class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-password" type="password" placeholder="Leave your message here..." />
-                    <p class="text-gray-600 text-xs italic">Make it as long and as crazy as you'd like</p>
+                    <textarea
+                        value={message}
+                        onInput={onMessageInput}
+                        class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-msg" placeholder="Leave your message here..." />
+                    {/* <p class="text-gray-600 text-xs italic">Make it as long and as crazy as you'd like</p> */}
                 </div>
+            </div>
+            <div class="flex flex-wrap">
+                <button onClick={sendMail} class="inline-flex bg-blue-700 items-center px-5 py-2.5 sm:mt-6 text-sm font-medium text-center text-white bg-primary-700 rounded-lg focus:ring-4 focus:ring-primary-200 dark:focus:ring-primary-900 hover:bg-primary-800">
+                    Submit
+                </button>
             </div>
             {/* </form> */}
         </Card>
