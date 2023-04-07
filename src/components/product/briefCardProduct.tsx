8b1ef78981a75495a0b1695be9a0151dcbcd85ab
dewkul <jirapat0811@gmail.com>
2023-04-07 19:21:04 +0700

    fix(product): img in card

--- src/components/product/briefCardProduct.tsx
+++ src/components/product/briefCardProduct.tsx
@@ -5,12 +5,15 @@ import { Product } from "../../schema/product";
 export default function BriefProductCard({ product }: Props) {
     const { name, discountedPrice, media } = product
     return (
-        <Card>
-            <img src={media[0].url} class="h-40 md:h-52 xl:h-60" />
-            <h4>{name}</h4>
-            <h3>{discountedPrice}</h3>
-            <Button class=" w-full">Add to cart</Button>
-        </Card>
+        <div class="max-w-sm">
+            <Card
+                imgSrc={media[0].url}
+            >
+                <h4>{name}</h4>
+                <h3>{discountedPrice}</h3>
+                <Button class=" w-full">Add to cart</Button>
+            </Card>
+        </div>
     )
 }
 
