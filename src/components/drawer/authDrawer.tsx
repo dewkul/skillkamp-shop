e8ae263ce658934e78f324c1d46bf759d8777db5
dewkul <jirapat0811@gmail.com>
2023-04-07 17:25:00 +0700

    draft(auth): add tab in drawer

--- src/components/drawer/authDrawer.tsx
+++ src/components/drawer/authDrawer.tsx
@@ -1,22 +1,51 @@
 import { useAuthCtx } from "../../hooks/useAuth";
 import Drawer from "../shared/drawer";
+import { Tab } from "@headlessui/react";
 
 export default function AuthDrawer() {
     const { isAuthDrawerOpen, setAuthDrawerOpen } = useAuthCtx()
     return (
         <Drawer header="Greeting!" isOpen={isAuthDrawerOpen} setOpen={setAuthDrawerOpen}>
-            <div className="border border-gray-300 shadow rounded-md p-4 max-w-sm w-full mx-auto">
-                <div className="animate-pulse flex space-x-4">
-                    <div className="rounded-full bg-gray-400 h-12 w-12"></div>
-                    <div className="flex-1 space-y-4 py-1">
-                        <div className="h-4 bg-gray-400 rounded w-3/4"></div>
-                        <div className="space-y-2">
-                            <div className="h-4 bg-gray-400 rounded"></div>
-                            <div className="h-4 bg-gray-400 rounded w-5/6"></div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <Tab.Group
+                defaultIndex={1}
+            >
+                <Tab.List class="flex space-x-1 rounded-xl bg-blue-900/20 p-1">
+                    <Tab
+                        key="register"
+                        class={
+                            'w-full rounded-lg py-2.5 text-sm font-medium leading-5 text-blue-700' +
+                            'ring-white ring-opacity-60 ring-offset-2 ring-offset-blue-400 focus:outline-none focus:ring-2' +
+                            //   'bg-white shadow'
+                            'text-blue-100 hover:bg-white/[0.12] hover:text-white'
+                        }
+                    >Register</Tab>
+                    <Tab
+                        key="logIn"
+                        class={
+                            'w-full rounded-lg py-2.5 text-sm font-medium leading-5 text-blue-700' +
+                            'ring-white ring-opacity-60 ring-offset-2 ring-offset-blue-400 focus:outline-none focus:ring-2' +
+                            //   'bg-white shadow'
+                            'text-blue-100 hover:bg-white/[0.12] hover:text-white'
+                        }
+                    >Log in</Tab>
+                </Tab.List>
+                <Tab.Panels class="mt-2">
+                    <Tab.Panel
+                        key="reg"
+                        class={
+                            'rounded-xl bg-white p-3' +
+                            'ring-white ring-opacity-60 ring-offset-2 ring-offset-blue-400 focus:outline-none focus:ring-2'
+                        }
+                    >Content 1</Tab.Panel>
+                    <Tab.Panel
+                        key="login"
+                        class={
+                            'rounded-xl bg-white p-3' +
+                            'ring-white ring-opacity-60 ring-offset-2 ring-offset-blue-400 focus:outline-none focus:ring-2'
+                        }
+                    >Content 2</Tab.Panel>
+                </Tab.Panels>
+            </Tab.Group>
         </Drawer>
     )
 }
