e1694e53fdc9dd03c6b5a48c2f267b3133f86ed2
dewkul <jirapat0811@gmail.com>
2023-04-08 17:15:20 +0700

    feat(auth): rm auth from idb

--- src/hooks/useAuth.tsx
+++ src/hooks/useAuth.tsx
@@ -1,6 +1,7 @@
 import { signal, computed } from "@preact/signals";
 import { ComponentChildren, createContext } from "preact";
 import { useContext, useState } from "preact/hooks";
+import { IDB } from "../lib/idb";
 
 const authData = signal<AuthData>({})
 
@@ -11,8 +12,11 @@ function useAuth() {
         authData.value = data
     }
 
-    const removeAuthData = () => {
-        authData.value = {}
+    const removeAuthData = async () => {
+        if (authData.value.email) {
+            await IDB.auth.where('email').equals(authData.value.email).delete()
+            authData.value = {}
+        }
     }
 
     const isLogIn = computed(() => Boolean(authData.value.token))
