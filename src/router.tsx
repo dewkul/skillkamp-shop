7a12e5da92c0131f854a1a1e90617cb93ef244ed
dewkul <jirapat0811@gmail.com>
2023-04-08 16:51:35 +0700

    feat(auth): read idb

--- src/router.tsx
+++ src/router.tsx
@@ -1,7 +1,21 @@
 import Router, { Route } from "preact-router"
+import { useEffect } from "preact/hooks"
+import { useAuthCtx } from "./hooks/useAuth"
+import { IDB } from "./lib/idb"
 import { HomePage, ContactPage, CollectionPage, ProductDetailPage } from "./pages"
 
 export default function Routes() {
+    const { setAuthData } = useAuthCtx()
+    useEffect(() => {
+        const queryAuth = async () => {
+            const auth = await IDB.auth.get(1)
+            console.log(auth)
+            if (auth)
+                setAuthData(auth)
+        }
+        queryAuth()
+    }, [])
+
     return (
         <div class="container mx-auto max-w-7xl">
             <Router>
