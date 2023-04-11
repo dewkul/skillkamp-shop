a04f878263e257cc33628d1b1294b0cdf18c53a0
dewkul <jirapat0811@gmail.com>
2023-04-11 21:57:58 +0700

    feat(cart): no checkout when empty

--- src/components/cart/subtotalCart.tsx
+++ src/components/cart/subtotalCart.tsx
@@ -2,7 +2,7 @@ import { route } from "preact-router"
 import { useCartCtx } from "../../hooks/useCart"
 
 export default function SubtotalCart() {
-    const { subtotalInCart, closeCartDrawer } = useCartCtx()
+    const { subtotalInCart, closeCartDrawer, totalQtyCart } = useCartCtx()
 
     const onCheckout = () => {
         route('/checkout')
@@ -11,19 +11,24 @@ export default function SubtotalCart() {
 
     return (
         <div class="border-t border-gray-200 px-4 py-6 sm:px-6">
-            <div class="flex justify-between text-base font-medium text-gray-900">
-                <p>Subtotal</p>
-                <p>${subtotalInCart}</p>
-            </div>
-            <p class="mt-0.5 text-sm text-gray-500">Shipping and taxes calculated at checkout.</p>
-            <div class="mt-6">
-                <button
-                    class="flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-indigo-700"
-                    onClick={onCheckout}
-                >
-                    Checkout
-                </button>
-            </div>
+            {
+                (totalQtyCart > 0) &&
+                <>
+                    <div class="flex justify-between text-base font-medium text-gray-900">
+                        <p>Subtotal</p>
+                        <p>${subtotalInCart}</p>
+                    </div>
+                    <p class="mt-0.5 text-sm text-gray-500">Shipping and taxes calculated at checkout.</p>
+                    <div class="mt-6">
+                        <button
+                            class="flex w-full items-center justify-center rounded-md border border-transparent bg-indigo-600 px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-indigo-700"
+                            onClick={onCheckout}
+                        >
+                            Checkout
+                        </button>
+                    </div>
+                </>
+            }
             <div class="mt-6 flex justify-center text-center text-sm text-gray-500">
                 <p>
                     or
@@ -37,5 +42,7 @@ export default function SubtotalCart() {
                 </p>
             </div>
         </div>
+
+
     )
 }
