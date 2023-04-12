228fb64942eaf33e17d12a302e7b6ff72a01b88a
dewkul <jirapat0811@gmail.com>
2023-04-12 10:41:46 +0700

    chore(style): brief card indent text

--- src/components/product/briefCardProduct.tsx
+++ src/components/product/briefCardProduct.tsx
@@ -5,16 +5,22 @@ import { Product } from "../../schema/product";
 
 
 export default function BriefProductCard({ product }: Props) {
-    const { name, discountedPrice, media, sku, urlPart } = product
+    const { name, discountedPrice, media, sku, urlPart, price } = product
     const { openProductInfoModal } = useProductCtx()
 
     const onClick = () => openProductInfoModal(sku)
     return (
         <Card
             imgSrc={media[0].url}
-        ><Link href={"/product/" + urlPart}>
-                <h4>{name}</h4>
-                <h3>{discountedPrice}</h3></Link>
+        >
+            <Link href={"/product/" + urlPart}>
+                <h4 class="text-lg font-medium uppercase">{name}</h4>
+                <div class="flex items-baseline mb-1 space-x-2 mt-4">
+                    <h3 class="text-xl text-primary font-semibold">$ {discountedPrice}</h3>
+                    {price != discountedPrice && <p class="text-base text-gray-400 line-through">${price}</p>}
+                </div>
+            </Link>
+
             <Button class=" w-full" onClick={onClick}>Add to cart</Button>
         </Card>
 
