5e95ff5d68ec46accd1065195c708a0e182b20a3
dewkul <jirapat0811@gmail.com>
2023-04-12 16:59:12 +0700

    refac(checkout): use grid layout

--- src/components/checkout/productCheckout.tsx
+++ src/components/checkout/productCheckout.tsx
@@ -3,29 +3,22 @@ import { CartItem } from "../../schema/cart"
 export default function ProductCheckout({ item }: Props) {
     const { name, discountedPrice, color, size, qty, fullUrl } = item
     return (
-        <div class="flex items-center hover:bg-gray-100 -mx-2 px-6 py-5">
-            <div class="flex w-2/5">
-                <div class="w-20">
-                    <img class="h-24" src={fullUrl} alt={name} />
-                </div>
-                <div class="flex flex-col justify-between ml-4 flex-grow">
-                    <span class="font-bold text-sm">{name}</span>
-                    <span class="text-xs">{color}</span>
-                    <span class="text-xs">{size}</span>
+        <div class="grid item-center grid-cols-4 lg:grid-cols-5 hover:bg-gray-100 -mx-2 px-6 py-5">
+            <div class="col-span-2">
+                <div class="grid grid-cols-2">
+                    <div class="w-20">
+                        <img class="h-24" src={fullUrl} alt={name} />
+                    </div>
+                    <div class="grid grid-col-3 justify-between ml-4 align-middle">
+                        <span class="font-bold text-sm">{name}</span>
+                        <span class="text-xs">{color}</span>
+                        <span class="text-xs">{size}</span>
+                    </div>
                 </div>
             </div>
-            <div class="flex justify-center w-1/5">
-                {/* <svg class="fill-current text-gray-600 w-3" viewBox="0 0 448 512"><path d="M416 208H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h384c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z" />
-                </svg> */}
-                <span>{qty}</span>
-                {/* <input class="mx-2 border text-center w-8" type="text" value="1" />
-
-                <svg class="fill-current text-gray-600 w-3" viewBox="0 0 448 512">
-                    <path d="M416 208H272V64c0-17.67-14.33-32-32-32h-32c-17.67 0-32 14.33-32 32v144H32c-17.67 0-32 14.33-32 32v32c0 17.67 14.33 32 32 32h144v144c0 17.67 14.33 32 32 32h32c17.67 0 32-14.33 32-32V304h144c17.67 0 32-14.33 32-32v-32c0-17.67-14.33-32-32-32z" />
-                </svg> */}
-            </div>
-            <span class="text-center w-1/5 font-semibold text-sm">${discountedPrice}</span>
-            <span class="text-center w-1/5 font-semibold text-sm">${(discountedPrice * qty).toFixed(2)}</span>
+            <span class="text-center m-auto font-semibold ">{qty}</span>
+            <span class="text-center m-auto font-semibold text-sm">${discountedPrice}</span>
+            <span class="text-center m-auto font-semibold text-sm lg:block hidden">${(discountedPrice * qty).toFixed(2)}</span>
         </div>
     )
 }
