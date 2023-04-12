6129021b559c3bde16ef3a5eb6b28021a81b4953
dewkul <jirapat0811@gmail.com>
2023-04-12 16:17:51 +0700

    chore(test): test env config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import preact from '@preact/preset-vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  define: {
+    'import.meta.vitest': 'undefined',
+  },
   plugins: [preact()],
   server: {
     proxy: {
--- vitest.config.ts
+++ vitest.config.ts
@@ -2,9 +2,12 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
+    setupFiles: ['./__tests__/test-setup.ts'],
+    environment: 'happy-dom',
+    includeSource: ['src/**/*.{ts.tsx}'],
     coverage: {
       provider: 'c8',
-      reporter: ['text', 'html'],
+      reporter: ['text', 'text-summary'],
     },
   },
 })
